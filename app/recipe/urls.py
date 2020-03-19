6b54c35e4f6b3111c6365f07f928bc6e56ebc573
Andrew <andrii.korchevnyi@zv-extern.fraunhofer.de>
2020-03-19 15:46:03 +0100

    tags and ingredients

---
+++ app/recipe/urls.py
@@ -0,0 +1,16 @@
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
+
+from recipe import views
+
+
+# /api/user/tag/1 .../api/user/tag/2
+router = DefaultRouter()
+router.register('tags', views.TagViewSet)
+router.register('ingredients', views.IngredientViewSet)
+
+app_name = 'recipe'
+
+urlpatterns = [
+    path('', include(router.urls))
+]
